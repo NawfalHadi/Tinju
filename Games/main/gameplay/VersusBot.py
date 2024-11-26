fad6a5bd0fa5312ee488c553123b32f6a2917ad1
NawfalHadi <nawfalhdi@gmail.com>
2024-11-26 09:19:00 +0800

    games : bot mechanis followed

--- Games/main/gameplay/VersusBot.py
+++ Games/main/gameplay/VersusBot.py
@@ -25,7 +25,7 @@ class VersusBot:
         self.screen = screen
 
         "=== BOT ATTRIBUTES ==="
-        # self.load_bots(bot_model)
+        self.load_bots(bot_model)
         self.bot_action = ACTIONS[0]
         self.bot_action_hit = True
         self.bot_hp = 100
@@ -42,7 +42,7 @@ class VersusBot:
         self.running = True
         
         "=== SOCKET ==="
-        self.show_loading = False
+        self.show_loading = True
         self.sock = None
 
         self.start_games()
@@ -60,7 +60,10 @@ class VersusBot:
 
         threading.Thread(target=self.receive_data, daemon=True).start()
         threading.Thread(target=self.start_controller, daemon=True).start()
-        # threading.Thread(target=self.generate_bot_actions, daemon=True).start()
+        
+        # Bisa diganti dengan countdown 3-2-1, and kinda like that
+        
+        threading.Thread(target=self.generate_bot_actions, daemon=True).start()
 
         # threading.Thread(target=)
 
@@ -83,12 +86,15 @@ class VersusBot:
             
             action_state = self.choose_action(state)
             self.bot_action = ACTIONS[action_state]
+
+            time.sleep(0.2)
             
             # print(self.bot_action)
-            font = pygame.font.Font(None, 60)
-            self.timer = screen.blit(font.render(self.bot_action, True, BLACK),
-                    (self.player_stamina_ui.rect.left, self.player_stamina_ui.rect.bottom))
-    
+            
+            # if(not self.show_loading):
+            #     print("test")
+            
+
     def receive_data(self):
         try : 
             conn, addr = self.sock.accept()
@@ -108,7 +114,7 @@ class VersusBot:
 
                     except ConnectionResetError:
                         print("error")
-        except:
+        except :
             pass
 
     def draw_interface(self):
@@ -130,7 +136,8 @@ class VersusBot:
         Attributes(SCREEN_MARGIN, SCREEN_MARGIN, player_hp, 40, RED).draw(screen, corner_bottomRight = 15)
         Attributes(self.screen.get_width() - (bot_hp + SCREEN_MARGIN), SCREEN_MARGIN, bot_hp, 40, RED).draw(screen, corner_bottomLeft = 15)
 
-        
+        font = pygame.font.Font(None, 60)
+        screen.blit(font.render(self.bot_action, True, BLACK), (self.player_stamina_ui.rect.left, self.player_stamina_ui.rect.bottom))
 
     def start_timer(self):
         text = "3:00"
@@ -139,7 +146,7 @@ class VersusBot:
                     (self.player_hp_bg.rect.right + 55, SCREEN_MARGIN + 15))
                     
         
-    def game_action(self):
+    def player_action_calculation(self):
         if self.player_action == "Jab":
             if self.player_stamina >= 20:
                 self.player_stamina -= 25
@@ -166,6 +173,34 @@ class VersusBot:
         self.bot_stamina = max(0, min(MAX_STM, self.bot_stamina))
         self.player_hp = max(0, min(MAX_HP, self.player_hp))
         self.player_stamina = max(0, min(MAX_STM, self.player_stamina))
+
+    def bot_action_calculation(self):
+        if self.bot_action == "Jab":
+            if self.bot_stamina >= 20:
+                self.bot_stamina -= 25
+                if self.player_action == "Idle":
+                    self.player_hp -= 2
+                elif self.player_action == "Guard":
+                    self.player_hp -= 1
+                elif self.player_action == "Jab":
+                    self.player_hp -= 3
+        
+        elif self.bot_action == "Guard":
+            self.bot_stamina += 1
+            self.bot_hp += 0.002
+            if self.player_action == "Jab":
+                self.bot_stamina += 2
+        
+        elif self.bot_action == "Idle":
+            self.bot_stamina += 2
+            self.bot_hp += 0.01
+            if self.player_action == "Jab":
+                self.bot_stamina += 4
+
+        self.bot_hp = max(0, min(MAX_HP, self.bot_hp))
+        self.bot_stamina = max(0, min(MAX_STM, self.bot_stamina))
+        self.player_hp = max(0, min(MAX_HP, self.player_hp))
+        self.player_stamina = max(0, min(MAX_STM, self.player_stamina))
         
         
         
@@ -186,10 +221,10 @@ class VersusBot:
             self.bot_hp_bg.draw(screen, corner_bottomLeft=15)
             self.bot_stamina_ui.draw(screen, corner_bottomLeft=15)
 
-            if (not self.show_loading):
-                
+            if (not self.show_loading):    
                 self.update_interface()
-                self.game_action()
+                self.bot_action_calculation()
+                self.player_action_calculation()
 
             pygame.display.update()
             pygame.time.Clock().tick(60)
