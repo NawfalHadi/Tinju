8bee91649d2b9bd0233b3610181cc219afbd040d
NawfalHadi <nawfalhdi@gmail.com>
2024-12-23 04:39:02 +0800

    assets : add some assets

---
+++ Games/main/bot/opponents/assets/ImagePath.py
@@ -0,0 +1,146 @@
+INFO_Q_POINTS_MIN = "main/assets/more/q_points_table_min.png"
+
+# IMAGE FOR THE PLACE BACKGROUND
+place_path = "main/assets/place/"
+PLACE_RING = f"{place_path}ring.png"
+PLACE_RING_SIDE = f"{place_path}ring_side.png"
+
+# IMAGE FOR THE PURPOSE OF TUTORIAL
+tutorial_path = "main/assets/tutorial/"
+TUTORIAL_WAIT_IMG = f"{tutorial_path}wait.png" #
+TUTORIAL_PAUSE_IMG = f"{tutorial_path}pause.png" 
+TUTORIAL_JAB_IMG = f"{tutorial_path}jab.png" #
+TUTORIAL_STRAIGHT_IMG = f"{tutorial_path}S.png" #
+TUTORIAL_LEFT_HOOK_IMG = f"{tutorial_path}LH.png" #
+TURORIAL_RIGHT_HOOK_IMG = f"{tutorial_path}RH.png" #
+TUTORIAL_LEFT_UPP_IMG = f"{tutorial_path}LUPP.png"#
+TUTORIAL_RIGHT_UPP_IMG = f"{tutorial_path}RUPP.png" #
+TUTORIAL_GUARD_IMG = f"{tutorial_path}G1.png" #
+TUTORIAL_GUARD_BODY_LEFT_IMG =f"{tutorial_path}GBL.png" #
+TUTORIAL_GUARD_BODY_RIGHT_IMG =f"{tutorial_path}GBR.png" #
+TUTORIAL_SLIP_LEFT_IMG = f"{tutorial_path}SL.png" #
+TUTORIAL_SLIP_RIGHT_IMG = f"{tutorial_path}SR.png" #
+TUTORIAL_LEFT_BODY_HOOK_IMG = f"{tutorial_path}LBH.png"#
+TUTORIAL_RIGHT_BODY_HOOK_IMG = f"{tutorial_path}RBH.png" #
+TUTORIAL_DUCK_1_IMG = f"{tutorial_path}D1.png" #
+TUTORIAL_DUCK_2_IMG = f"{tutorial_path}D2.png" #
+TUTORIAL_DONE_IMG = f"{tutorial_path}DONE.png" #
+
+
+bot_path = "main/assets/bot/"
+BOT_JAB_IMG = [f"{bot_path}bot_jab_1.png", f"{bot_path}bot_jab_2.png", f"{bot_path}bot_jab_3.png"]
+BOT_STRAIGHT_IMG = [f"{bot_path}bot_straight_1.png", f"{bot_path}bot_straight_2.png", f"{bot_path}bot_straight_3.png"]
+BOT_LH_IMG = [f"{bot_path}bot_lh_1.png", f"{bot_path}bot_lh_2.png", f"{bot_path}bot_lh_3.png"]
+BOT_RH_IMG = [f"{bot_path}bot_rh_1.png", f"{bot_path}bot_rh_2.png", f"{bot_path}bot_rh_3.png"]
+BOT_LJ_IMG = [f"{bot_path}bot_LJ_1.png", f"{bot_path}bot_LJ_2.png", f"{bot_path}bot_LJ_3.png"]
+BOT_LS_IMG = [f"{bot_path}bot_LS_1.png", f"{bot_path}bot_LS_2.png", f"{bot_path}bot_LS_3.png"]
+BOT_LBH_IMG = [f"{bot_path}bot_LBH_1.png", f"{bot_path}bot_LBH_2.png", f"{bot_path}bot_LBH_3.png"]
+BOT_RBH_IMG = [f"{bot_path}bot_RBH_1.png", f"{bot_path}bot_RBH_2.png", f"{bot_path}bot_RBH_3.png"]
+BOT_LEFT_UPP_IMG = [f"{bot_path}bot_left_upp_1.png", f"{bot_path}bot_left_upp_2.png", f"{bot_path}bot_left_upp_3.png"]
+BOT_RIGHT_UPP_IMG = [f"{bot_path}bot_right_upp_1.png", f"{bot_path}bot_right_upp_2.png", f"{bot_path}bot_right_upp_3.png"]
+BOT_LEFT_SLIP_IMG = [f"{bot_path}bot_left_slip_1.png", f"{bot_path}bot_left_slip_2.png", f"{bot_path}bot_left_slip_3.png"]
+BOT_RIGHT_SLIP_IMG = [f"{bot_path}bot_right_slip_1.png", f"{bot_path}bot_right_slip_2.png", f"{bot_path}bot_right_slip_3.png"]
+BOT_GUARD_IMG = [f"{bot_path}bot_guard_1.png", f"{bot_path}bot_guard_2.png", f"{bot_path}bot_guard_3.png"]
+BOT_GUARD_BODY_L_IMG = [f"{bot_path}bot_guard_body_l_1.png", f"{bot_path}bot_guard_body_l_2.png", f"{bot_path}bot_guard_body_l_3.png"]
+BOT_GUARD_BODY_R_IMG = [f"{bot_path}bot_guard_body_r_1.png", f"{bot_path}bot_guard_body_r_2.png", f"{bot_path}bot_guard_body_r_3.png"]
+BOT_DUCK_IMG = [f"{bot_path}bot_duck_1.png"]
+BOT_IDLE_IMG = [f"{bot_path}bot_idle_1.png", f"{bot_path}bot_idle_2.png", f"{bot_path}bot_idle_3.png"]
+
+
+ACTIONS_IMAGE = {
+    "Jab": BOT_JAB_IMG,
+    "Straight": BOT_STRAIGHT_IMG,
+    "Left_Hook": BOT_LH_IMG,
+    "Right_Hook": BOT_RH_IMG,
+    "Low_Jab": BOT_LJ_IMG,
+    "Low_Straight": BOT_LS_IMG,
+    "Left_BodyHook": BOT_LBH_IMG,
+    "Right_BodyHook": BOT_RBH_IMG,
+    "Left_Uppercut": BOT_LEFT_UPP_IMG,
+    "Right_Uppercut": BOT_RIGHT_UPP_IMG,
+    "Slip_Left": BOT_LEFT_SLIP_IMG, #
+    "Slip_Right": BOT_RIGHT_SLIP_IMG,
+    "Guard": BOT_GUARD_IMG,
+    "Guard_LeftBody": BOT_GUARD_BODY_L_IMG,
+    "Guard_RightBody": BOT_GUARD_BODY_R_IMG,
+    "Duck": BOT_DUCK_IMG, #
+    "Idle": BOT_IDLE_IMG,
+}
+
+padwork_path = "main/assets/padwork/"
+PW_IDLE_IMG = [f"{padwork_path}PW_I.png"]
+PW_JAB_IMG = [f"{padwork_path}PW_J_1.png", f"{padwork_path}PW_J_2.png"]
+PW_STRAIGHT_IMG = [f"{padwork_path}PW_S_1.png", f"{padwork_path}PW_S_2.png"]
+PW_LH_IMG = [f"{padwork_path}PW_LH_1.png", f"{padwork_path}PW_LH_2.png"]
+PW_RH_IMG = [f"{padwork_path}PW_RH_1.png"]
+PW_LJ_IMG = [f"{padwork_path}PW_LJ_1.png", f"{padwork_path}PW_LJ_2.png"]
+PW_LS_IMG = [f"{padwork_path}PW_LS_1.png", f"{padwork_path}PW_LS_2.png"]
+PW_LBH_IMG = [f"{padwork_path}PW_LBH_1.png", f"{padwork_path}PW_LBH_2.png"]
+PW_RBH_IMG = [f"{padwork_path}PW_RBH_1.png", f"{padwork_path}PW_RBH_2.png"]
+PW_LEFT_UPP_IMG = [f"{padwork_path}PW_left_upp_1.png", f"{padwork_path}PW_left_upp_2.png"]
+PW_RIGHT_UPP_IMG = [f"{padwork_path}PW_right_upp_1.png", f"{padwork_path}PW_right_upp_2.png"]
+PW_LEFT_SLIP_IMG = [f"{padwork_path}PW_left_slip_1.png"]
+PW_RIGHT_SLIP_IMG = [f"{padwork_path}PW_right_slip_1.png"]
+PW_GUARD_IMG = [f"{padwork_path}PW_guard_1.png", f"{padwork_path}PW_guard_2.png"]
+PW_GUARD_BODY_L_IMG = [f"{padwork_path}PW_guard_body_l_1.png", f"{padwork_path}PW_guard_body_l_2.png"]
+PW_GUARD_BODY_R_IMG = [f"{padwork_path}PW_guard_body_r_1.png", f"{padwork_path}PW_guard_body_r_2.png"]
+PW_DUCK_IMG = [f"{padwork_path}PW_duck_1.png", f"{padwork_path}PW_duck_2.png"]
+
+PADWROKS_ACTIONS_IMAGE = {
+    "Idle": PW_IDLE_IMG,
+    "Jab": PW_JAB_IMG,
+    "Straight": PW_STRAIGHT_IMG,
+    "Left_Hook": PW_LH_IMG,
+    "Right_Hook": PW_RH_IMG,
+    "Low_Jab": PW_LJ_IMG,
+    "Low_Straight": PW_LS_IMG,
+    "Left_BodyHook": PW_LBH_IMG,
+    "Right_BodyHook": PW_RBH_IMG,
+    "Left_Uppercut": PW_LEFT_UPP_IMG,
+    "Right_Uppercut": PW_RIGHT_UPP_IMG,
+    "Slip_Left": PW_LEFT_SLIP_IMG,
+    "Slip_Right": PW_RIGHT_SLIP_IMG,
+    "Guard": PW_GUARD_IMG,
+    "Guard_LeftBody": PW_GUARD_BODY_L_IMG,
+    "Guard_RightBody": PW_GUARD_BODY_R_IMG,
+    "Duck": PW_DUCK_IMG,
+}
+
+player_path = "main/assets/player/"
+P_IDLE_IMG = [f"{player_path}P_I.png"]
+P_J_IMG = [f"{player_path}P_J.png"]
+P_LJ_IMG = [f"{player_path}P_LJ.png"]
+P_S_IMG = [f"{player_path}P_S.png"]
+P_LS_IMG = [f"{player_path}P_LS.png"]
+P_LH_IMG = [f"{player_path}P_LH.png"]
+P_LBH_IMG = [f"{player_path}P_LBH.png"]
+P_RH_IMG = [f"{player_path}P_RH.png"]
+P_RBH_IMG = [f"{player_path}P_RBH.png"]
+P_LUPP_IMG = [f"{player_path}P_LUPP.png"]
+P_RUPP_IMG = [f"{player_path}P_RUPP.png"]
+P_G_IMG = [f"{player_path}P_G.png"]
+P_SL_IMG = [f"{player_path}P_SL.png"]
+P_SR_IMG = [f"{player_path}P_SR.png"]
+P_D_IMG = [f"{player_path}P_D.png"]
+P_GLB_IMG = [f"{player_path}P_GLB.png"]
+P_GRB_IMG = [f"{player_path}P_GRB.png"]
+
+PLAYER_ACTIONS_IMAGE = {
+    "Idle": P_IDLE_IMG,
+    "Jab": P_J_IMG,
+    "Straight": P_S_IMG,
+    "Left_Hook": P_LH_IMG,
+    "Right_Hook": P_RH_IMG,
+    "Low_Jab": P_LJ_IMG,
+    "Low_Straight": P_LS_IMG,
+    "Left_BodyHook": P_LBH_IMG,
+    "Right_BodyHook": P_RBH_IMG,
+    "Left_Uppercut": P_LUPP_IMG,
+    "Right_Uppercut": P_RUPP_IMG,
+    "Slip_Left": P_SL_IMG,
+    "Slip_Right": P_SR_IMG,
+    "Guard": P_G_IMG,
+    "Guard_LeftBody": P_GLB_IMG,
+    "Guard_RightBody": P_GRB_IMG,
+    "Duck": P_D_IMG,
+}
